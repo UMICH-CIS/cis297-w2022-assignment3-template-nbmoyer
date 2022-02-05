d5fce55c763dcc51a74dae8150dbd423027f571b
nbmoyer <71909436+nbmoyer@users.noreply.github.com>
2022-02-05 11:15:16 -0500

    Created base Account class

--- BankApplication/Program.cs
+++ BankApplication/Program.cs
@@ -12,4 +12,58 @@ namespace BankApplication
         {
         }
     }
+
+    class Account
+    {
+        private decimal balance;
+        public Account(decimal accountBalance)
+        {
+            if (accountBalance >= 0)
+            {
+                balance = accountBalance;
+            }
+            else
+            {
+                throw new Exception("Initial balance cannot be less than zero");
+            }
+        }
+
+        public void Credit(decimal credit)
+        {
+            balance += credit;
+        }
+
+        public void Debit(decimal debit)
+        {
+            if (debit <= balance)
+            {
+                balance -= debit;
+            }
+            else
+            {
+                Console.WriteLine("Debit amount exceeded account balance.");
+            }
+        }
+
+        public decimal Balance
+        {
+            get { return balance; }
+        }
+    }
+
+    class savingsaccount : account
+    {
+        private decimal interestrate;
+        private decimal balance;
+        public savingsaccount(decimal intrate, account balance)
+        {
+            interestrate = intrate;
+            balance = balance.balance;
+        }
+    }
+
+    class checkingaccount : account
+    {
+
+    }
 }
